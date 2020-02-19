c6c97b74ac7138e3a2dba7a59fa4e222fc56e0b5
Drew <uni@glaroam2-168-41.wireless.gla.ac.uk>
2020-02-19 12:40:34 +0000

    Student app started

---
+++ participants_wanted/student/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+class Experiment(models.Model):
+    name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
+
+#Superuser
+#uname: ITcw
+#p: us
+
---
+++ participants_wanted/student/views.py
@@ -0,0 +1,24 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from .forms import UserForm
+
+
+def index(request):
+    return render(request, 'student/index.html')
+
+
+def register(request):
+    registered = False
+    if request.method == 'POST':
+        user_form = UserForm(request.POST)
+        if user_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+            registered = True
+        else:
+            print(user_form.errors)
+    else:
+        user_form = UserForm()
+    print(user_form)
+    return render(request, 'student/register.html', context={'user_form': user_form, 'registered': registered})
