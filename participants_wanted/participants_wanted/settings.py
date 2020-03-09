16aa9195806cce7667edbbb8c8a0d37105cbb656
Jinsik Yun <ryanyun7@gmail.com>
2020-03-09 02:14:14 +0000

    crispy setting added

--- participants_wanted/participants_wanted/settings.py
+++ participants_wanted/participants_wanted/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'student',
     'experimenter',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +130,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
