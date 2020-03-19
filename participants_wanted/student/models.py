6b236e7996891b1ffe092046f534b1b159f8159b
Jinsik Yun <ryanyun7@gmail.com>
2020-03-19 21:15:30 +0000

    profile with css, demsurv with dropdown

--- participants_wanted/student/models.py
+++ participants_wanted/student/models.py
@@ -3,12 +3,19 @@ from django.contrib.auth.models import User
 from experimenter.models import Experiment
 
 class Demsurv(models.Model):
+    SEX = (('M', 'Male'), ('F', 'Female'), ('N', 'Others'))
+    AGE = (('17', '17'), ('18', '18'), ('19', '19'), ('20', '20'), ('21', '21'), ('22', '22'), ('23', '23'), ('24', '24'), ('25', '25'), ('26', '26'), ('27', '27'), ('28', '28'), ('29', '29'), ('30', '30'))
+    LANGUAGE = (('E', 'English'), ('F', 'French'), ('G', 'German'), ('K', 'Korean'))
+    COUNTRY = (('uk', 'UK'), ('eu', 'EU'), ('others', 'Others'))
+    EDUCATION = (('s', 'School'), ('c', 'College'), ('u', 'Undergraduate'), ('p', 'Postgraduate'))
+
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    sex = models.CharField(max_length=128)
-    age = models.CharField(max_length=128)
-    language = models.CharField(max_length=128)
-    country = models.CharField(max_length=128)
-    education = models.CharField(max_length=128)
+    sex = models.CharField(max_length=128, choices=SEX)
+    age = models.CharField(max_length=128, choices=AGE)
+    language = models.CharField(max_length=128, choices=LANGUAGE)
+    country = models.CharField(max_length=128, choices=COUNTRY)
+    education = models.CharField(max_length=128, choices=EDUCATION)
     picture = models.ImageField(upload_to='profile_images', blank=True)
 
     def __str__(self):
